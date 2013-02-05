e0386b296c028119310ebd89722c5d43db1c5fb1
Guillaume Racicot <gufideg@gmail.com>
2013-02-05 10:39:56 -0500

    separate contextmanager

---
+++ src/circlesboxcontextmanager.h
@@ -0,0 +1,15 @@
+#ifndef CIRCLESBOXCONTEXTMANAGER_H
+#define CIRCLESBOXCONTEXTMANAGER_H
+
+#include "contextmanager.h"
+
+class CirclesBoxContextManager : public ContextManager
+{
+public:
+	
+	
+private:
+	std::vector<CircleObject*> _circles;
+};
+
+#endif // CIRCLESBOXCONTEXTMANAGER_H
